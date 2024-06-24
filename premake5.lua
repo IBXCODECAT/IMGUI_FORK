ac34eb8709a8c3af09a75c23577d6881ac3ecea9
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2024-06-24 18:56:28 -0500

    Make Static lib

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,12 @@
 project "ImGui"
     kind "StaticLib"
+
+    -- Static runtime is required for static libraries
+    staticruntime "on"
+
+    -- C++ Language Settings and Standard
     language "C++"
+    cppdialect "C++17"
 
     targetdir ("bin/" .. outputDir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
@@ -24,8 +30,11 @@ project "ImGui"
 
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
     
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
